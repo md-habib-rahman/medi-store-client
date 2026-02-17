94e146d71c97a926151e60574147f74296dc76c3
Md. Habibur Rahman Bappy <habib.rahman@live.com>
2026-02-17 16:52:16 +0600

    new commit to vercel

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,8 +2,8 @@ import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
 	// baseURL: process.env.NEXT_PUBLIC_BASE_API!,
-	// baseURL: "https://medi-store-backend-rust.vercel.app/api/auth",
-	baseURL: "https://rumedi-server.mdhabib.me/api/auth",
+	baseURL: "https://medi-store-backend-rust.vercel.app/api/auth",
+	// baseURL: "https://rumedi-server.mdhabib.me/api/auth",
 	fetchOptions: {
 
 		credentials: "include"
