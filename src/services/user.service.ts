285ec31bebc42cf60bee3d827e791878252e04b8
Md. Habibur Rahman Bappy <habib.rahman@live.com>
2026-02-22 11:33:07 +0600

    rechecking session data with clg

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -29,7 +29,7 @@ export const getSession = async () => {
 		// const { data: session } = await authClient.getSession()
 
 		const session = await res.json()
-		// console.log(session)
+		console.log(session)
 
 		// console.log(session)
 		return { data: session, error: null }
